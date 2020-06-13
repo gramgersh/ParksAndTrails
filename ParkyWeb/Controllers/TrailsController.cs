bc5d81cafa53082d834cc27683de53e6b70110ab
Scott Erickson <erickson@fsautomation.com>
2020-06-13 05:09:40 -0700

    Section 10.66 - Trail Controller

---
+++ ParkyWeb/Controllers/TrailsController.cs
@@ -0,0 +1,96 @@
+using ParkyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ParkyWeb.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ParkyWeb.Models.ViewModel;
+
+namespace ParkyWeb.Controllers
+{
+    public class TrailsController : Controller
+    {
+        private readonly INationalParkRepository _npRepo;
+        private readonly ITrailRepository _trailRepo;
+
+        public TrailsController(INationalParkRepository npRepo, ITrailRepository trailRepo)
+        {
+            _npRepo = npRepo;
+            _trailRepo = trailRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new Trail() { });
+        }
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            IEnumerable<NationalPark> npList = await _npRepo.GetAllAsync(SD.NationalParkAPIPath);
+            TrailsVM objVM = new TrailsVM()
+            {
+                NationalParkList = npList.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+            if (id == null)
+            {
+                // This will be true for insert/create
+                return View(objVM);
+            }
+
+            //flow will come here for update
+            objVM.Trail = await _trailRepo.GetAsync(SD.TrailAPIPath, id.GetValueOrDefault());
+            if (objVM.Trail == null)
+            {
+                return NotFound();
+            }
+            return View(objVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(TrailsVM obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (obj.Trail.Id == 0)
+                {
+                    await _trailRepo.CreateAsync(SD.TrailAPIPath, obj.Trail);
+                }
+                else
+                {
+                    await _trailRepo.UpdateAsync(SD.TrailAPIPath + obj.Trail.Id, obj.Trail);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
+        public async Task<IActionResult> GetAllTrail()
+        {
+            return Json(new { data = await _trailRepo.GetAllAsync(SD.TrailAPIPath) });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var status = await _trailRepo.DeleteAsync(SD.TrailAPIPath, id);
+            if (status)
+            {
+                return Json(new { success = true, message = "Delete Successful" });
+            }
+            else
+            {
+                return Json(new { success = false, message = "Delete Failed" });
+            }
+        }
+    }
+}
