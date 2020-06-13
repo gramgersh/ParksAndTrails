63330bb484036852fce9f756ed4abdda59349297
Scott Erickson <erickson@fsautomation.com>
2020-06-13 07:27:35 -0700

    Section 11.73 - Home Page Index View Model and Home Controller

---
+++ ParkyWeb/Models/ViewModel/IndexVM.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace ParkyWeb.Models.ViewModel
+{
+    public class IndexVM
+    {
+        public IEnumerable<NationalPark> NationalParkList { get; set; }
+        public IEnumerable<Trail> TrailList { get; set; }
+    }
+}
